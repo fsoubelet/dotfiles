2202af1b444f3cdcc0c91a9b2d1557cb943f1cd1
Felix Soubelet <19598248+fsoubelet@users.noreply.github.com>
2018-06-22 16:05:52 +0200

    Corrected spaceship prompt

--- zsh/oh_my_zsh_settings.sh
+++ zsh/oh_my_zsh_settings.sh
@@ -2,13 +2,14 @@ export ZSH = $HOME/.oh-my-zsh
 export UPDATE_ZSH_DAYS = 7
 
 
-ZSH_THEME = "powerlevel9k/powerlevel9k"
+ZSH_THEME="spaceship"
 
-POWERLEVEL9K_SHORTEN_DIR_LENGTH = 2
-POWERLEVEL9K_SHORTEN_DIR_STRATEGY = "truncate_middle"
+# Set Spaceship ZSH as a prompt
+autoload -U promptinit; promptinit
+prompt spaceship
 
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS = (os_icon dir vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS = (status root_indicator ssh background_jobs history)
+# Enable flags completion
+source $(dirname $(gem which colorls))/tab_complete.sh
 
 DISABLE_AUTO_TITLE="true"
 COMPLETION_WAITING_DOTS="true"
--- zsh/spaceship.sh
+++
@@ -1,6 +0,0 @@
-# Set Spaceship ZSH as a prompt
-autoload -U promptinit; promptinit
-prompt spaceship
-
-# Enable flags completion 
-source $(dirname $(gem which colorls))/tab_complete.sh
