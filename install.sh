8697e96efed95b0e9dcb6d07dfbb5abbae0e1912
Felix Soubelet <19598248+fsoubelet@users.noreply.github.com>
2018-07-09 21:44:57 +0200

    Added spaceship install

--- install.sh
+++ install.sh
@@ -92,6 +92,14 @@ else
 fi
 
 
+##############################
+# Preparing spaceship prompt #
+##############################
+dotfiles_echo "Preparing Spaceship prompt."
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+
 #################################################
 # Setting up useful python packages as well as  #
 #################################################
