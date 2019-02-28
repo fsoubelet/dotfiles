3d6d1e5e75425482d20217f224948157532fedac
Felix Soubelet <felix.soubelet@protonmail.com>
2019-02-28 14:31:28 +0100

    Custom lxplus.

--- zsh/oh_my_zsh_settings.sh
+++ zsh/oh_my_zsh_settings.sh
@@ -4,9 +4,6 @@ export UPDATE_ZSH_DAYS=7
 
 # Setting prompt appearance
 ZSH_THEME="oxide"
-#ZSH_THEME="hyperzsh"
-#ZSH_THEME="spaceship"
-
 
 # Enable flags completion // file seems to be missing
 #source $(dirname $(which colorls))/tab_complete.sh
@@ -15,16 +12,16 @@ DISABLE_AUTO_TITLE="true"
 COMPLETION_WAITING_DOTS="true"
 ENABLE_CORRECTION="true"
 
-HISTFILE= ~/.zsh_history
-HIST_STAMPS="dd-mm-yyyy"
-HISTSIZE=5000
-SAVEHIST=5000
+#HISTFILE= ~/.zsh_history
+#HIST_STAMPS="dd-mm-yyyy"
+#HISTSIZE=5000
+#SAVEHIST=5000
 #setopt APPEND_HISTORY                       # Allow multiple terminal sessions to all append to one zsh
 #setopt HIST_EXPIRE_DUPS_FIRST               # When duplicates are entered, get rid of the duplicates first when we hit $HISTSIZE
 
 
 # Getting some colors into the ls output
-export LS_COLORS='di=1;94:ln=94:so=32:pi=33:ex=32:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30:*.py=34:*.madx=33:*.seq=33:*.png=95:*.sh=1;33:*.md=1;37:*.pdf=31:'
+export LS_COLORS='di=1;94:ln=96:so=32:pi=33:ex=32:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30:*.py=34:*.madx=33:*.seq=33:*.png=95:*.sh=1;33:*.md=1;37:*.pdf=31:'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 
