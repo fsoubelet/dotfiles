7722528dd85956d67c9ed93e559097da2e52b138
Felix Soubelet <felix.soubelet@protonmail.com>
2024-10-10 12:03:49 +0200

    minimal plugins setup

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,11 +1,14 @@
 # Plugins file for Antidote - comments begin with "#"
 
 # Load some OMZ plugins - supported by antidote
-ohmyzsh/ohmyzsh path:plugins/gh
-ohmyzsh/ohmyzsh path:plugins/colored-man-pages
+# ohmyzsh/ohmyzsh path:plugins/colored-man-pages
 
 # Fast syntax highlighting
-zdharma-continuum/fast-syntax-highlighting kind:defer
+zdharma-continuum/fast-syntax-highlighting
+
+# Completion initialization
+mattmc3/ez-compinit
 
 # Zsh autosuggestions
+# zsh-users/zsh-completions
 zsh-users/zsh-autosuggestions
