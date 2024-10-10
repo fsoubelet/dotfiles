54c8829e8a9177c40cdbf3d968f3ae125e198f04
Felix Soubelet <felix.soubelet@protonmail.com>
2024-10-10 11:26:16 +0200

    as a zsh file

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,5 +1,9 @@
 # Plugins file for Antidote - comments begin with "#"
 
+# Load some OMZ plugins - supported by antidote
+ohmyzsh/ohmyzsh path:plugins/gh
+ohmyzsh/ohmyzsh path:plugins/colored-man-pages
+
 # Fast syntax highlighting
 zdharma-continuum/fast-syntax-highlighting kind:defer
 
