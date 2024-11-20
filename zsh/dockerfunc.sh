c47387a9c8920a41ba3c98407f5e2daf52f71622
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-20 09:06:23 +0100

    typo

--- zsh/dockerfunc.sh
+++ zsh/dockerfunc.sh
@@ -2,10 +2,9 @@
 # Docker related aliases
 # -------------------------------------------------------------------
 alias docklean='docker rm $(docker ps -a -q -f status=exited)'                           # Delete all CONTAINERS that have a status of exited.
-alias dock='docker rmi $(docker images --filter "dangling=true" --quiet --no-trunc)'     # Forcefully remove  DANGLING IMAGES.
+alias dock='docker rmi $(docker images --filter "dangling=true" --quiet --no-trunc)'     # Forcefully remove DANGLING IMAGES.
 alias dockrmi='docker rmi $(docker images -q) -f'                                        # Forcefully remove ALL IMAGES.
 alias dockapocalypse='docker system prune -a'                                            # DANGEROUS. Will delete everything from docker
-alias lzd='lazydocker'
 
 
 # -------------------------------------------------------------------
