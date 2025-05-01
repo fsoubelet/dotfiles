3c5f668b1be4dd76df13e814e166fa95ac0d99ba
Felix Soubelet <felix.soubelet@protonmail.com>
2025-05-01 20:34:47 +0200

    this order is a fix for Error unknow MAMBA_EXE

--- zsh/condasetup.sh
+++ zsh/condasetup.sh
@@ -11,8 +11,17 @@ else
     fi
 fi
 unset __conda_setup
-
-if [ -f "/Users/felixsoubelet/.miniforge/etc/profile.d/mamba.sh" ]; then
-    . "/Users/felixsoubelet/.miniforge/etc/profile.d/mamba.sh"
-fi
 # <<< conda initialize <<<
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba shell init' !!
+export MAMBA_EXE='/Users/felixsoubelet/.miniforge/bin/mamba';
+export MAMBA_ROOT_PREFIX='/Users/felixsoubelet/.miniforge';
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+fi
+unset __mamba_setup
+# <<< mamba initialize <<<
