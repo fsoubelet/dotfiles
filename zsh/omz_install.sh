eb3dc7f3a34d6ca1134354b42d8a55e49c30ef01
Felix Soubelet <felix.soubelet@protonmail.com>
2025-10-24 11:49:41 +0200

    suggestions and cleanups

--- zsh/omz_install.sh
+++ zsh/omz_install.sh
@@ -1,29 +1,42 @@
 #! /bin/bash
 
-# Checking if Homebrew installation exists (executable is here)
+# ---------------------------- #
+# Install Oh-My-Zsh if Absent  #
+# ---------------------------- #
+
+func_header "Status" "Installing Oh-My-Zsh."
+
+# Checking if oh-my-zsh installation exists first
 if [[ -d "${HOME}/.oh-my-zsh" ]]
-then
-    echo "Oh-my-zsh is already installed."
-else
-    echo "No installation found. Downloading and installing oh-my-zsh."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    then
+        echo "Oh-my-zsh is already installed."
+    else
+        echo "No installation found. Downloading and installing oh-my-zsh."
+        RUNZSH=no CHSH=no KEEP_ZSHRC=yes \
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+# -------------------------- #
+# Install Plugins if Absent  #
+# -------------------------- #
 
-# Checking for fast-syntax-highlighting plugin (install if necessary)
-if [[ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting" ]]
-then
-    echo "The fast-syntax-highlighting plugin is already installed."
-else
-    echo "Downloading and installing fast-syntax-highlighting."
-    sh -c "$(git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting)"
-fi
+func_header "Status" "Installing Plugins."
 
-# Checking for zsh-autosuggestions plugin (install if necessary)
-if [[ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]]
-then
-    echo "The zsh-autosuggestions plugin is already installed."
-else
-    echo "Downloading and installing zsh-autosuggestions."
-    sh -c "$(git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)"
-fi
+# Modify this list as desired
+plugins=(
+  "https://github.com/zdharma-continuum/fast-syntax-highlighting"
+  "https://github.com/zsh-users/zsh-autosuggestions"
+)
+
+for repo in "${plugins[@]}"; do
+    name=$(basename "$repo")
+    dir="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/${name}"
+
+    # Check if plugin directory exists first
+    if [[ -d "$dir" ]]; then
+        echo "$name already installed."
+    else
+        echo "Installing $name plugin."
+        git clone --depth=1 "$repo" "$dir"
+    fi
+done
