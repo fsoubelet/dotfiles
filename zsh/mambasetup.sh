a9c0a22f9949c6b8c5c3e354d049d476249a26f4
Felix Soubelet <felix.soubelet@protonmail.com>
2025-10-24 11:16:32 +0200

    use HOME, guard against installation existence before execution

--- zsh/mambasetup.sh
+++ zsh/mambasetup.sh
@@ -1,27 +1,27 @@
 # >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/felixsoubelet/.miniforge/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/Users/felixsoubelet/.miniforge/etc/profile.d/conda.sh" ]; then
-        . "/Users/felixsoubelet/.miniforge/etc/profile.d/conda.sh"
+if [ -x "$HOME/.miniforge/bin/conda" ]; then
+    __conda_setup="$("$HOME/.miniforge/bin/conda" shell.zsh hook 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    elif [ -f "$HOME/.miniforge/etc/profile.d/conda.sh" ]; then
+        source "$HOME/.miniforge/etc/profile.d/conda.sh"
     else
-        export PATH="/Users/felixsoubelet/.miniforge/bin:$PATH"
+        export PATH="$HOME/.miniforge/bin:$PATH"
     fi
+    unset __conda_setup
 fi
-unset __conda_setup
 # <<< conda initialize <<<
 
 # >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba shell init' !!
-export MAMBA_EXE='/Users/felixsoubelet/.miniforge/bin/mamba';
-export MAMBA_ROOT_PREFIX='/Users/felixsoubelet/.miniforge';
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__mamba_setup"
-else
-    alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+if [ -x "$HOME/.miniforge/bin/mamba" ]; then
+    export MAMBA_EXE="$HOME/.miniforge/bin/mamba"
+    export MAMBA_ROOT_PREFIX="$HOME/.miniforge"
+    __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__mamba_setup"
+    else
+        alias mamba="$MAMBA_EXE"
+    fi
+    unset __mamba_setup
 fi
-unset __mamba_setup
 # <<< mamba initialize <<<
